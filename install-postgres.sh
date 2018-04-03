8b33ecb164c6400d2dd45ec21ba145713a1e33ed
Karthik M A M <karthik_m_a_m@outlook.com>
2018-04-04 04:01:58 +0530

    Add pgcli Installation Step for Postgres

--- install-postgres.sh
+++ install-postgres.sh
@@ -3,6 +3,7 @@ echo -e "Installing Postgres... \n\n\n"
 
 # Installation Documentation: https://www.codementor.io/engineerapart/getting-started-with-postgresql-on-mac-osx-are8jcopb
 brew install postgresql
+brew install pgcli
 
 
 # Reload Configs
