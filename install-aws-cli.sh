d96e97b6de6bda4b0963b646c7c70927fe4b5754
Karthik M A M <karthik_m_a_m@outlook.com>
2019-03-05 12:27:58 +0530

    add config for aws cli

---
+++ install-aws-cli.sh
@@ -0,0 +1,13 @@
+echo -e "Installing AWS CLI... \n\n\n"
+
+
+# Install AWS CLI
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+
+unzip awscli-bundle.zip
+
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+
+# Reload Configs
+source ~/.bash_profile
