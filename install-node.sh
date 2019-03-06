9d476a457581cd2ba17ae3367931b8f0709ec676
KarthikMAM <karthik_m_a_m@outlook.com>
2019-03-06 16:24:37 +0530

    allow npm bin packages to be run automatically

--- install-node.sh
+++ install-node.sh
@@ -9,6 +9,14 @@ brew install node
 npm install -g n
 npm install -g yarn
 
+# Add exports to run npm binaries/packages
+echo '
+
+# Add exports to run npm binaries/packages
+export PATH="$PATH:$(npm -g bin)"
+export PATH="$PATH:$(npm bin)"
+
+' >> ~/.bash_profile.sh
 
 # Reload Configs
 source ~/.bash_profile
